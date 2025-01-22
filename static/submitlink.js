6d7d190b4b2116e8e5fb503eb3a30de225bcc58c
Akshay Kannan <akannan@scaledcognition.com>
2025-01-22 11:50:13 -0800

    js

---
+++ static/submitlink.js
@@ -0,0 +1,20 @@
+function expand(td) {
+  td.innerHTML = td.title;
+}
+function rename(link_id, link_name) {
+  const newName =  prompt("Name for " + link_name);
+  if (newName !== null) {
+    const formData = new FormData();
+    formData.append("id", link_id);
+    formData.append("newName", newName);
+    formData.append("action", "rename");
+    try {
+      const response = fetch("/update_link", {
+        method: "POST",
+        body: formData,
+      }).then(() => window.location.reload());
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
